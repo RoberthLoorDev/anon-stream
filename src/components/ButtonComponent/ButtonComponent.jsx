1dbdfa6cc4f737e623eee7bc56f9a548610e93b3
RoberthLoorDev <dayvidlife@gmail.com>
2024-10-22 11:12:40 -0500

    style: modifications size components

---
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import stylesButton from "./ButtonComponent.module.css";
+
+const variantButton = {
+     primary: { backgroundColor: "#597BDA", color: "#121820" },
+     secondary: { backgroundColor: "#20242F", color: "white" },
+     delete: { backgroundColor: "#8F1218", color: "#A9ACB3" },
+};
+
+export default function ButtonComponent({ text, icon, width, height, alt, type, variant = "secondary" }) {
+     return (
+          <button
+               type={type}
+               className={stylesButton.button}
+               style={{
+                    ...variantButton[variant],
+                    width: width ? width : "100%",
+                    height: height ? height : "48px",
+                    fontWeight: 700,
+               }}
+          >
+               {icon ? <img src={icon} className={stylesButton.iconInButton} alt={alt} /> : ""}
+               <span className={stylesButton.textInButton}>{text}</span>
+          </button>
+     );
+}
+
+ButtonComponent.propTypes = {
+     text: PropTypes.string.isRequired,
+     icon: PropTypes.any,
+     width: PropTypes.string,
+     height: PropTypes.string,
+     alt: PropTypes.string,
+     type: PropTypes.string,
+     variant: PropTypes.oneOf(["primary", "secondary", "detele"]),
+};
