6d789989ca0cb482480e9d9ce60bc517cdbd53f9
RoberthLoorDev <dayvidlife@gmail.com>
2024-10-22 19:06:27 -0500

    add: methods for signin using google and password

--- src/supabase/auth.js
+++ src/supabase/auth.js
@@ -17,3 +17,42 @@ export const handleSignUp = async (email, password) => {
           console.error(error);
      }
 };
+
+export const handleSignIn = async (email, password) => {
+     try {
+          const { data, error } = await supabase.auth.signInWithPassword({
+               email,
+               password,
+          });
+
+          if (error) {
+               console.error(error.message);
+               throw new Error("Error al iniciar sesion", error.message);
+          }
+
+          console.log("Inicio se sesion exitoso", data);
+     } catch (error) {
+          console.error(error);
+     }
+};
+
+//sign up using google account
+export const signUpGoogle = async () => {
+     try {
+          const { data, error } = await supabase.auth.signInWithOAuth({
+               provider: "google",
+               options: {
+                    redirectTo: "http://localhost:5173/home",
+               },
+          });
+
+          if (error) {
+               console.log("Error al registrarse con Google", error);
+               return error.message;
+          }
+
+          console.log("Usuario registrado", data);
+     } catch (error) {
+          console.error(error);
+     }
+};
