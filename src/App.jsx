774509efea9226e76f73e3358501d83c3019deb4
RoberthLoorDev <dayvidlife@gmail.com>
2024-11-02 13:07:26 -0500

    add: route protection

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import SignInPage from "./pages/SignInPage/SignInPage";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import SignUpPage from "./pages/SignInPage/SignUpPage";
+import { AuthProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./context/ProtectedRoute";
 
 function App() {
      return (
-          <Routes>
-               <Route path="/login" element={<SignInPage />} />
-               <Route path="/signup" element={<SignUpPage />} />
-               <Route path="/home" element={<HomePage />} />
-          </Routes>
+          <AuthProvider>
+               <Routes>
+                    <Route path="/login" element={<SignInPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route
+                         path="/home"
+                         element={
+                              <ProtectedRoute>
+                                   <HomePage />
+                              </ProtectedRoute>
+                         }
+                    />
+               </Routes>
+          </AuthProvider>
      );
 }
 
