48a729187d57538833f137fe28e1ea0a695d31e7
RoberthLoorDev <dayvidlife@gmail.com>
2024-11-08 21:37:03 -0500

    add: switch component

--- src/components/ToggleSwitch/SwitchComponent.jsx
+++ src/components/ToggleSwitch/SwitchComponent.jsx
@@ -1,12 +1,20 @@
 import styles from "./SwitchComponent.module.css";
+import PropTypes from "prop-types";
 
-export default function SwitchComponent() {
+export default function SwitchComponent({ value, name, onChange }) {
+     console.log(value);
+
      return (
           <label className={styles.toggleSwitch}>
-               <input type="checkbox" />
+               <input type="checkbox" value={value} onChange={(event) => onChange(event)} name={name} />
                <div className={styles.toggleSwitchBackground}>
                     <div className={styles.toggleSwitchHandle}></div>
                </div>
           </label>
      );
 }
+SwitchComponent.propTypes = {
+     value: PropTypes.bool.isRequired,
+     name: PropTypes.string.isRequired,
+     onChange: PropTypes.func,
+};
